da924fc0645b9e8c78900f71c3d879daca0957b6
Daniel Dantas <danielddf@gmail.com>
2024-07-02 12:04:00 -0300

    Alterações com comandos customizados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 ///<reference types="cypress" />
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () =>{
 
 beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta')
 });
 
 afterEach(() => {
@@ -34,5 +35,28 @@ afterEach(() => {
 
         cy.get('.woocommerce-error').should('contain','A senha fornecida para o e-mail daniel.teste@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, daniel.teste (não é daniel.teste? Sair)')
+    });
+
+    it('Deve fazer login com sucesso - Fixture', () => {
+       cy.fixture('perfil').then(dados =>{
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, daniel.teste (não é daniel.teste? Sair)')
+       })
+    });
+
+    it.only('Deve fazer login com sucesso - usando comandos customizados', () => {
+        cy.login('daniel.teste@teste.com.br', 'teste')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, daniel.teste (não é daniel.teste? Sair)')
+        
     });
 })
